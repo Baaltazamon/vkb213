fb7709712abf9e52408560f8d8e84e75016c4567
Илья Палатов <3gpabbiu4eji@mail.ru>
2021-03-11 09:04:39 +0300

    bt

--- vkb/Program.cs
+++ vkb/Program.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Newtonsoft.Json.Linq;
 using VkNet;
@@ -19,6 +20,8 @@ namespace vkb
         {
             VkApi vk = new VkApi();
             WebClient webclient = new WebClient() {Encoding = Encoding.UTF8};
+            VkParameters param = new VkParameters();
+            param.Add<string>("group_id", "201002643");
             vk.Authorize(new ApiAuthParams
             {
                 ApplicationId = 7690451,
@@ -27,14 +30,53 @@ namespace vkb
                 Settings = Settings.All
             });
 
-            GetUserInfo(vk);
+            //GetUserInfo(vk, param);
+            MainBotModule(vk, webclient, param);
             //Console.ReadKey();
         }
 
-        static public void GetUserInfo(VkApi vk)
+        public static void MainBotModule(VkApi vk, WebClient webclient, VkParameters param)
         {
-            VkParameters param = new VkParameters();
-            param.Add<string>("group_id", "51223244");
+            dynamic responseLongApi = JObject.Parse(vk.Call("groups.getLongPollServer", param).RawJson);
+            var json = string.Empty;
+            var url = string.Empty;
+            while (true)
+            {
+                url = string.Format("{0}?act=a_check&key={1}&ts={2}&wait=5", responseLongApi.response.server.ToString(),
+                    responseLongApi.response.key.ToString(),
+                    json != String.Empty?JObject.Parse(json)["ts"].ToString():responseLongApi.response.ts.ToString());
+                json = webclient.DownloadString(url);
+                var jsonMsg = json.IndexOf(":[]}", StringComparison.Ordinal) > -1 ? "" : $"{json} \n";
+                var col = JObject.Parse(json)["updates"].ToList();
+                foreach (var item in col)
+                {
+                    if (item["type"].ToString() == "message_new")
+                    {
+                        string token =
+                            "cdedaa6a97eca371a457a0e2107ff52d5191179575174c0e42ebc3b176938e95496fbadcd7de9d9191535";
+                        string urlBotMsg =
+                            $"https://api.vk.com/method/messages.send?v=5.50&access_token={token}&user_id=";
+                        Console.WriteLine(item.ToString());
+                        string answer = string.Empty;
+                        JToken name = JObject.Parse(vk.Call("users.get", param).RawJson)["response"];
+                        string c = "";
+                        foreach (dynamic t in name)
+                        {
+                            c = t.first_name;
+                        }
+
+                        answer = "Здарова, " + c;
+                        webclient.DownloadString(String.Format(urlBotMsg + "{0}&message={1}", item["object"]["user_id"],
+                            answer));
+                        Thread.Sleep(200);
+                    }
+                }
+            }
+        }
+        public static void GetUserInfo(VkApi vk, VkParameters param)
+        {
+            
+           
             param.Add<string>("offset", "0");
             param.Add<string>("count", "100");
 
